d1e057e82c9821e5e457bb0e0c1b941a89b6ffa6
Joan Sala <joan.sala.pardo@gmail.com>
2024-11-28 11:51:57 -0500

    saving work

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -5,27 +5,36 @@ import './CartItem.css';
 
 const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
+  console.log(cart)
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+    cart.forEach((item) => {
+        totalAmount += item.cost * item.quantity;
+    });
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name:item.name, quantity:item.quantity +1}));
   };
 
   const handleDecrement = (item) => {
-   
+   dispatch(updateQuantity({name:item.name, quantity:item.quantity-1}));
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item));
   };
 
   // Calculate total cost based on quantity for an item
